465c3eade55f5a9096c84564894f153f57ab4385
李鹏龙 <819508292@qq.com>
2022-02-01 10:59:23 +0800

    新增服务提供方，模拟两台节点的集群

---
+++ spring-cloud-alibaba-sentinel/provider2/src/test/java/com/springcloudalibaba/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudalibaba.sample;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
