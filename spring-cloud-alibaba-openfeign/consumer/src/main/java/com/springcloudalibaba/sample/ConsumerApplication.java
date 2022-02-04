85ccb1df74c3d3c656fe1e4a0b176e53156b4285
李鹏龙 <819508292@qq.com>
2022-02-04 22:48:01 +0800

    启动类增加@EnableFeignClients注解，标记启用OpenFeign

--- spring-cloud-alibaba-openfeign/consumer/src/main/java/com/springcloudalibaba/sample/ConsumerApplication.java
+++ spring-cloud-alibaba-openfeign/consumer/src/main/java/com/springcloudalibaba/sample/ConsumerApplication.java
@@ -3,13 +3,14 @@ package com.springcloudalibaba.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients // 启用OpenFeign
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
-
 }
