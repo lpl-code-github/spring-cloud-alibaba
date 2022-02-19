a1d358a85c121893492e24bc8db02cd1203b67a5
李鹏龙 <819508292@qq.com>
2022-02-19 16:23:07 +0800

    测试负载均衡

--- spring-cloud-alibaba-dubbo/provider2/src/main/java/com/springcloudalibaba/sample/service/TestServiceImpl.java
+++ spring-cloud-alibaba-dubbo/provider2/src/main/java/com/springcloudalibaba/sample/service/TestServiceImpl.java
@@ -4,6 +4,7 @@ import com.springcloudalibaba.dubbo.entity.Params;
 import com.springcloudalibaba.dubbo.entity.Result;
 import com.springcloudalibaba.dubbo.service.TestService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * @author lipenglong
@@ -12,9 +13,12 @@ import org.apache.dubbo.config.annotation.Service;
  */
 @Service
 public class TestServiceImpl implements TestService {
+    @Value("${server.port}")
+    private String serverPort;
+
     @Override
     public String dubboTest(String message) { // 服务提供者具体实现的方法
-        return "我是服务提供者，已接受到消息：" + message;
+        return "我是服务提供者"+ serverPort +"，已接受到消息：" + message;
     }
 
     @Override
